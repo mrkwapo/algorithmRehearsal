d4dfb07a7b2fcb934651862cf0a458dc0b067827
mrkwapo <mrkwapo@gmail.com>
2019-09-24 22:32:34 -0400

    completed

---
+++ javascript/basic/ReturnLargestNumbersinArrays.js
@@ -0,0 +1,28 @@
+// Basic Algorithm Scripting: Return Largest Numbers in Arrays
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+  //create an empty array for the largest numbers
+  var largest = [];
+  //access all the numbers
+  //sort each array
+  for (var i = 0; i < arr.length; i++) {
+    var sorted = arr[i].sort(function(a, b) {
+      return a - b;
+    });
+    //console.log(sorted)
+    //had to turn the sliced number into an integer type
+    largest.push(parseInt(sorted.slice(-1)));
+  }
+  console.log('The largest numbers from each array are: ' + largest);
+  return largest;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1]
+]);
