dd2b52baa42c1853021ba8e0c73b796bfe43bc46
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-09-25 13:49:14 -0400

    Create RepeataStringRepeataString.js

---
+++ javascript/basic/RepeataStringRepeataString.js
@@ -0,0 +1,15 @@
+//Basic Algorithm Scripting: Repeat a String Repeat a String
+//Repeat a given string str(first argument) for numtimes (second argument). Return an empty string if numis not a positive number.
+
+function repeatStringNumTimes(str, num) {
+  // create 
+var arr = [];
+if(num)
+  for(var i = 0; i < num;i++){
+  arr.push(str);
+  }
+   console.log(arr.join(""));
+  return arr.join(""); 
+}
+
+repeatStringNumTimes("abc", 3);
