a539f722ca4f93671d1e822475e66cbf3612c2dc
mrkwapo <mrkwapo@gmail.com>
2019-10-06 22:29:10 -0400

    completed

--- javascript/intermediate/SeekandDestroy.js
+++ javascript/intermediate/SeekandDestroy.js
@@ -1,28 +1,26 @@
-//Intermediate Algorithm Scripting: Seek and Destroy
-//You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+function destroyer() {
+  //storing the value of the first argument in a variable called args
+  var args = Array.prototype.slice.call(arguments, 0, 1);
+  //storing the value of the rest of the arguements in a variable called restOfArgs
+  var restOfArgs = Array.prototype.slice.call(arguments, 1);
 
-//Note
-//You have to use the argumentsobject.
-
-function destroyer(arr) {
-  // Remove all the values
-  //console.log(arr[0]);
-  //console.log(arr[1]);
-  //console.log(arr[2]);
-  
-var arr1=[];
-
-  for(var i = 0; i < arr[0]; i++){
-    arr1.push(arr.slice(i));
-
+  //accessing all the elements in args[0]
+  for (var i = 0; i < args[0].length; i++) {
+    //console.log(restOfArgs[i]);
+    //accessing all the elements in restOfArgs
+    for (var j = 0; j < restOfArgs.length; j++) {
+      //comparing elements in the array called args[0] to the rest of the arguments. if founf then delete from args[0]
+      if (args[0][i] === restOfArgs[j]) {
+        delete args[0][i];
+      }
+    }
   }
-console.log("This is the value of arr[0]: "+arr1);
-console.log("This is the value of arr[1]: "+arr[1]);
-console.log("This is the value of arr[2]: "+arr[2]);
-  return arr;
+  //console.log(args[0]);
+  //When we return args[0] now it will return [ 7, <2 empty items>, 7, <2 empty items> ] or [7,,7,,]
+  //To get rid of the empty spaces we need to filter out the empty strings using Boolean
+  console.log(args[0].filter(Boolean));
+  return args[0].filter(Boolean);
+  //console.log(restOfArgs);
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-
-
-//not done
+destroyer([7, 2, 3, 7, 2, 3], 2, 3);
