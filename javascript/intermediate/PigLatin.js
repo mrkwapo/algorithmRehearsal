bbf0322f9f62f0ae1354eebec8dc2a97a180e143
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-10-07 16:18:09 -0400

    suedo coded

---
+++ javascript/intermediate/PigLatin.js
@@ -0,0 +1,21 @@
+//Translate the provided string to pig latin.
+
+//Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+
+//If a word begins with a vowel you just add "way" to the end.
+
+//Input strings are guaranteed to be English words in all lowercase
+
+function translatePigLatin(str) {
+//
+//create a variable that contains all vowels
+//iterate through each letter in str
+//slice letters before the first vowel and append it to the the same word
+//after appending consonants append "ay" to the string.
+//but if a word begins with a vowel then just add "way" to the end of the word
+//  
+
+  return str;
+}
+
+translatePigLatin("consonant");
