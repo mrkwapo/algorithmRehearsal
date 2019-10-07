8de9b6e9bf58fa4c85ae2de9e6c1571c091a292b
mrkwapo <mrkwapo@gmail.com>
2019-10-07 17:43:45 -0400

    syncing

---
+++ javascript/intermediate/WhereArtThou2.js
@@ -0,0 +1,19 @@
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  console.log(collection);
+  console.log(source);
+
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName(
+  [
+    { first: 'Romeo', last: 'Montague' },
+    { first: 'Mercutio', last: null },
+    { first: 'Tybalt', last: 'Capulet' }
+  ],
+  { last: 'Capulet' }
+);
