278dd4482d9d3588ad8aedfc3ed35cd55920081e
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-10-12 16:51:12 -0400

    Update SumAllNumbersinaRange.js

--- javascript/intermediate/SumAllNumbersinaRange.js
+++ javascript/intermediate/SumAllNumbersinaRange.js
@@ -1,7 +1,7 @@
 //Intermediate Algorithm Scripting: Sum All Numbers in a Range
 //We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
 
-//The lowest number will not always come first.
+//Note: The lowest number will not always come first.
 
 
 function sumAll(arr) {
